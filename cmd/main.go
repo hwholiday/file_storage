9628b437ecdd7fa732cd86496cb614c34dc089cf
hwholiday <heweiholiday@gmail.com>
2020-07-14 14:13:22 +0800

    添加配置文件

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,19 @@
-package cmd
+package main
+
+import (
+	"filesrv/conf"
+	"filesrv/library/log"
+	"filesrv/library/utils"
+	"flag"
+)
+
+func main() {
+	flag.Parse()
+	if err := conf.Init(); err != nil {
+		panic(err)
+	}
+	log.NewLogger(conf.Conf.Log)
+	utils.QuitSignal(func() {
+		log.GetLogger().Info("filesrv exit success")
+	})
+}
--- conf/config.go
+++ conf/config.go
@@ -1,1 +1,42 @@
 package conf
+
+import (
+	"filesrv/library/database/minio"
+	"filesrv/library/log"
+	"flag"
+	"github.com/BurntSushi/toml"
+)
+
+var (
+	confPath string
+	Conf     = new(Config)
+)
+
+type Config struct {
+	Development bool
+	SnowFlakeId int64
+	Log         *log.Options
+	Minio       *minio.Config
+	Http        *httpConf
+	Grpc        *grpcConf
+}
+
+type httpConf struct {
+	Port int
+}
+type grpcConf struct {
+	Port int
+}
+
+func init() {
+	flag.StringVar(&confPath, "conf", "", "default config path")
+}
+
+func Init() error {
+	return local()
+}
+
+func local() (err error) {
+	_, err = toml.DecodeFile(confPath, &Conf)
+	return
+}
